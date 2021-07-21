d9f87a3342c369a68f291350c0c31e69a28735d5
psg1415 <psg1415@naver.com>
2021-07-21 18:14:16 +0900

    jquery

---
+++ html/exam/dropdown_re.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('.menu_open').click(function() {
+    const nav = $('nav')
+    const menuOpen = $('.menu_open')
+    if (nav.hasClass('on')) {
+      nav.removeClass('on');
+      menuOpen.text('메뉴열기');
+    } else {
+      nav.addClass('on');
+      menuOpen.text('메뉴닫기');
+    }
+  });
+});
